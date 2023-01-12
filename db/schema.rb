82b094f0d086df192f5777d5a31905e01fcc88f5
myrgorod <constaha1208@gmail.com>
2023-01-12 22:18:46 +0200

    Add piperclip for image upload

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_12_174035) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_12_194232) do
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
